03640eeb94994063303a08ab5b81343e55bffa01
Niklas Hamann <niklas.hamann@pm.me>
2025-11-03 22:54:03 +0100

    WIP

--- DevTools/Values/Microcode.py
+++ DevTools/Values/Microcode.py
@@ -1,26 +1,40 @@
 # micro instructions
 INSTRUCTION_LOAD = 1 << 0
-ENABLE_PC = 1 << 1
-RAM_ADDRESS_LOAD = 1 << 2
-RAM_READ = 1 << 3
-RAM_WRITE = 1 << 4
+INSTRUCTION_END = 1 << 1
 
-DECREMENT_SP = 1 << 7
-ENABLE_SP = 1 << 8
+PC_INCREMENT = 1 << 2
+PC_ADDRESS_OUT = 1 << 3
+PC_DATA_OUT = 1 << 4
+PC_WRITE = 1 << 5
 
-SET_AS_DESTINATION_ADDRESS = 1 << 9
+SP_INCREMENT = 1 << 6
+SP_DECREMENT = 1 << 7
+SP_ADDREESS_OUT = 1 << 8
+SP_DATA_OUT = 1 << 9
+SP_WRITE = 1 << 10
 
-HALT = 1 << 10
-INSTRUCTION_READ = 1 << 11
+ALU_OPERATION = 1 << 11 # from bit 11 to bit 14
+ALU_OUT = 1 << 15
+
+GPR_ADDRESS_OUT = 1 << 16
+GPR_DATA_OUT = 1 << 17
+GPR_WRITE = 1 << 18
+
+RAM_READ = 1 << 19
+RAM_WRITE = 1 << 20
+
+INTEERRUPT_REQUEST_ACKNOWLEDGE = 1 << 21
+BUS_GRANT = 1 << 22
+
+HALT = 1 << 23
+
+
 
 def GenerateALUOperation(pOperation: int) -> int:
     return (pOperation & 0x7) << 12
 
-ENABLE_RAM_OUTPUT = 1 << 15
-ENABLE_SOURCE_REGISTER = 1 << 16
+
 
 def GenerateRegister(pRegister: int) -> int:
     return (pRegister & 0x1F) << 17
 
-REGISTER_LOAD = 1 << 22
-REGISTER_STORE = 1 << 23
